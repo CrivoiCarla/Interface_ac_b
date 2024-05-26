411148758fe47e63309e3d9a0cf01bda30940f46
CRIVOI Carla <carla.crivoi@renault.com>
2024-05-26 22:25:14 +0300

    Start/Stop buttons backend

--- interface/STT.py
+++ interface/STT.py
@@ -7,15 +7,13 @@ class SpeechToText:
         self.speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
         self.done = False
 
-    # def recognized_cb(self, evt: speechsdk.SpeechRecognitionEventArgs):
-    #     print('RECOGNIZED: {}'.format(evt.result.text))
-    #     self.callback_tts(evt.result.text)
+
 
     def stop_cb(self, evt: speechsdk.SessionEventArgs):
         print('CLOSING on {}'.format(evt))
         self.done = True
 
-    def start_continuous_recognition_async(self):
+    def start_continuous_recognition_async(self , stop ):
         self.speech_recognizer.recognized.connect(self.callback_tts)
         self.speech_recognizer.session_stopped.connect(self.stop_cb)
         self.speech_recognizer.canceled.connect(self.stop_cb)
@@ -23,12 +21,11 @@ class SpeechToText:
         result_future.get()
         print('Continuous Recognition is now running, say something.')
 
-        # while not self.done:
-        #     print('type "stop" then enter when done')
-        #     stop = input()
-        #     if stop.lower() == "stop":
-        #         print('Stopping async recognition.')
-        #         self.speech_recognizer.stop_continuous_recognition_async()
-        #         break
-        #
-        # print("Recognition stopped")
+        while not self.done:
+            with open('stop.txt', 'r') as file:
+                text = file.read()
+
+            if 'stop' in text:
+                print('Stopping async recognition.')
+                self.speech_recognizer.stop_continuous_recognition_async()
+                break
--- interface/app.py
+++ interface/app.py
@@ -12,16 +12,6 @@ from STT import SpeechToText
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-class SpeechRecognitionThread(threading.Thread):
-    def __init__(self, speech_config):
-        super().__init__()
-        self.speech_config = speech_config
-        self.text_to_speech = TextToSpeech(speech_config)
-        self.speech_to_text = SpeechToText(speech_config, self.text_to_speech.speak_text_async)
-        self.running = False
-
-    def run(self):
-        self.speech_to_text.start_continuous_recognition_async()
 
 @app.route('/')
 def index():
@@ -38,22 +28,21 @@ def handle_disconnect():
 @app.route('/start', methods=['POST'])
 def start():
     global speech_recognition_thread
-    if not speech_recognition_thread or not speech_recognition_thread.running:
-        speech_recognition_thread = SpeechRecognitionThread(speech_config)
-        speech_recognition_thread.start()
-        return "Speech recognition started."
-    else:
-        return "Speech recognition already running."
+    speech_recognition_thread = True
+    with open('stop.txt', 'w') as file:
+        file.write("")
+    text_to_speech = TextToSpeech(speech_config)
+    speech_to_text = SpeechToText(speech_config, text_to_speech.speak_text_async)
+    speech_to_text.start_continuous_recognition_async( not speech_recognition_thread)
+
+    return "It's run"
+
 
 @app.route('/stop', methods=['POST'])
 def stop():
-    global speech_recognition_thread
-    if speech_recognition_thread and speech_recognition_thread.is_alive():
-        SpeechToText.speech_recognizer.stop_continuous_recognition_async()
-        speech_recognition_thread = None
-        return "Speech recognition stopped."
-    else:
-        return "Speech recognition is not running."
+    with open('stop.txt', 'w') as file:
+        file.write("stop")
+    return "STOP"
 
 def emit_recognized_text(text):
     socketio.emit('recognized_text', {'text': text})
@@ -61,5 +50,5 @@ def emit_recognized_text(text):
 if __name__ == '__main__':
     speech_key, service_region = "16200612f681448ca6cd8870d82ea638", "eastus"
     speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
-    speech_recognition_thread = None
+    speech_recognition_thread = False
     socketio.run(app, debug=True , allow_unsafe_werkzeug=True)
